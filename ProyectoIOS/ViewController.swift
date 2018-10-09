b311a060500ed486a5e0f73be5a9af64e9ccacff
Eduardo <eduardo.dlr@gmail.com>
2018-10-08 20:42:08 -0500

    Initial Commit

---
+++ ProyectoIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoIOS
+//
+//  Created by Eduardo De La Rosa Rivera on 08/10/18.
+//  Copyright © 2018 ERR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
